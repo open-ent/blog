eb7a70379f38fdff178621e5ae40c4a078269532
Clement Creusat <clement.creusat@gmail.com>
2023-11-16 12:57:03 +0100

    chore: #WB2-598, update angularjs

--- webpack.config.js
+++ webpack.config.js
@@ -1,38 +1,34 @@
-var webpack = require('webpack');
-var path = require('path');
+var webpack = require("webpack");
+var path = require("path");
 
 module.exports = {
-    context: path.resolve(__dirname, './src/main/resources/public/'),
-    entry: {
-        application: './ts/app.ts',
-        behaviours: './ts/behaviours.ts',
-//        "explorer": './ts/explorer.ts',
-//        "explorer.agent": './ts/explorer.agent.ts'
-    },
-    output: {
-        filename: './[name].js',
-    },
-    externals: {
-        "entcore/entcore": "entcore",
-        "entcore": "entcore",
-        "moment": "entcore",
-        "underscore": "entcore",
-        "jquery": "entcore",
-        "angular": "angular",
-//        "ode-ts-client": 'window.entcore["ode-ts-client"]',
-//        "ode-ngjs-front": 'window.entcore["ode-ngjs-front"]'
-    },
-    resolve: {
-        modulesDirectories: ['node_modules'],
-        extensions: ['', '.ts', '.js']
-    },
-    devtool: "source-map",
-    module: {
-        loaders: [
-            {
-                test: /\.ts$/,
-                loader: 'ts-loader'
-            }
-        ]
-    }
-}
+  context: path.resolve(__dirname, "./src/main/resources/public/"),
+  entry: {
+    application: "./ts/app.ts",
+    behaviours: "./ts/behaviours.ts",
+  },
+  output: {
+    filename: "./[name].js",
+  },
+  externals: {
+    "entcore/entcore": "entcore",
+    entcore: "entcore",
+    moment: "entcore",
+    underscore: "entcore",
+    jquery: "entcore",
+    angular: "angular",
+  },
+  resolve: {
+    modulesDirectories: ["node_modules"],
+    extensions: ["", ".ts", ".js"],
+  },
+  devtool: "source-map",
+  module: {
+    loaders: [
+      {
+        test: /\.ts$/,
+        loader: "ts-loader",
+      },
+    ],
+  },
+};
