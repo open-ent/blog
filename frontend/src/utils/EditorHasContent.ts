2f4551edd57462792217313f67f0bbe084a6f534
Jean-Christophe <jean-christophe.benoit@opendigitaleducation.com>
2024-05-15 16:12:07 +0200

    fix: #WB-2929, improved empty editor content detection

--- frontend/src/utils/EditorHasContent.ts
+++ frontend/src/utils/EditorHasContent.ts
@@ -2,6 +2,7 @@ export const isEmptyEditorContent = (json: any) => {
   if (
     Array.isArray(json?.content) &&
     json.content.length === 1 &&
+    "paragraph" === json.content[0].type &&
     !Object.prototype.hasOwnProperty.call(json.content[0], "content")
   ) {
     return true;
