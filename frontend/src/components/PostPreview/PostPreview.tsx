e249d0572d575bab664d9da1c63320fe2c3de7c8
RomuC <romuald.coulaud@gmail.com>
2024-07-02 16:51:05 +0200

    feat: #WB-2999 add open modal on click (#113)

--- frontend/src/components/PostPreview/PostPreview.tsx
+++ frontend/src/components/PostPreview/PostPreview.tsx
@@ -12,10 +12,12 @@ import {
   ReactionChoice,
   ReactionSummary,
   ViewsCounter,
+  ViewsModal,
   getThumbnail,
+  useToggle,
 } from "@edifice-ui/react";
 import clsx from "clsx";
-import { ReactionSummaryData } from "edifice-ts-client";
+import { ReactionSummaryData, ViewsDetails } from "edifice-ts-client";
 import { useTranslation } from "react-i18next";
 import { useNavigate } from "react-router-dom";
 
@@ -23,6 +25,7 @@ import { PostPreviewActionBar } from "./PostPreviewActionBar";
 import { useActionDefinitions } from "~/features/ActionBar/useActionDefinitions";
 import { PostDate } from "~/features/Post/PostDate";
 import { Post, PostState } from "~/models/post";
+import { loadPostViewsDetails } from "~/services/api";
 import { useBlog } from "~/services/queries";
 import { useBlogState, useStoreUpdaters } from "~/store";
 import { getAvatarURL } from "~/utils/PostUtils";
@@ -70,6 +73,10 @@ export const PostPreview = ({
   const [summaryContentPlain, setSummaryContentPlain] = useState<string>("");
   const [mediaURLs, setMediaURLs] = useState<string[]>([]);
 
+  // Variables for views modal
+  const [viewsDetails, setViewsDetails] = useState<ViewsDetails | undefined>();
+  const [viewsModalOpen, toggleViewsModalOpen] = useToggle(false);
+
   // Number of media to display on the preview card
   const MAX_NUMBER_MEDIA_DISPLAY = 3;
 
@@ -85,6 +92,22 @@ export const PostPreview = ({
     }
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [actionBarPostId]);
+
+  const loadViewsDetails = async () => {
+    const details = await loadPostViewsDetails(post._id);
+    setViewsDetails(details);
+  };
+
+  const handleViewsClick = async () => {
+    if (!viewsDetails) {
+      await loadViewsDetails();
+    }
+    toggleViewsModalOpen(true);
+  };
+
+  const handleViewsModalClose = useCallback(async () => {
+    toggleViewsModalOpen(false);
+  }, [toggleViewsModalOpen]);
 
   useEffect(() => {
     if (sidebarHighlightedPost?._id === post._id) {
@@ -144,148 +167,164 @@ export const PostPreview = ({
       <Card
         className={classes}
         isSelected={actionBarPostId === post._id}
-        onClick={() => {
-          handleCardClick();
-        }}
         onSelect={() => {
           handleCardSelect();
         }}
         isSelectable={true}
         ref={cardRef}
+        isClickable={false}
       >
-        <div className="d-flex gap-12">
-          <div className="blog-post-user-image">
-            <Avatar
-              alt={t("blog.author.avatar")}
-              size="md"
-              src={getAvatarURL(post.author.userId)}
-              variant="circle"
-            />
-          </div>
-          <div className="d-flex flex-column gap-2">
-            <div className="d-flex align-items-center">
-              <h4 className="post-preview-title">{post.title}</h4>
-              {post.state === PostState.DRAFT &&
-                (creator || manager || contrib) && (
+        <div
+          onClick={() => {
+            handleCardClick();
+          }}
+          role="button"
+          tabIndex={0}
+        >
+          <div className="d-flex gap-12">
+            <div className="blog-post-user-image">
+              <Avatar
+                alt={t("blog.author.avatar")}
+                size="md"
+                src={getAvatarURL(post.author.userId)}
+                variant="circle"
+              />
+            </div>
+            <div className="d-flex flex-column gap-2">
+              <div className="d-flex align-items-center">
+                <h4 className="post-preview-title">{post.title}</h4>
+                {post.state === PostState.DRAFT &&
+                  (creator || manager || contrib) && (
+                    <Badge
+                      className="ms-8"
+                      variant={{
+                        type: "notification",
+                        level: "info",
+                        color: "text",
+                      }}
+                    >
+                      {t("draft")}
+                    </Badge>
+                  )}
+                {post.state === PostState.SUBMITTED && (
                   <Badge
-                    className="ms-8"
+                    className="blog-post-badge ms-8"
                     variant={{
                       type: "notification",
-                      level: "info",
+                      level: "warning",
                       color: "text",
                     }}
                   >
-                    {t("draft")}
+                    {t("blog.filters.submitted")}
                   </Badge>
                 )}
-              {post.state === PostState.SUBMITTED && (
-                <Badge
-                  className="blog-post-badge ms-8"
-                  variant={{
-                    type: "notification",
-                    level: "warning",
-                    color: "text",
-                  }}
-                >
-                  {t("blog.filters.submitted")}
-                </Badge>
-              )}
-            </div>
-            <div className="text-gray-700 small column-gap-12 d-flex flex-column flex-md-row align-items-md-center">
-              <span>{post.author.username}</span>
-              <PostDate post={post} shortDisplay={true}></PostDate>
-            </div>
-          </div>
-        </div>
-        <Card.Body space="0">
-          <div className="d-flex flex-fill flex-column gap-16 pt-16">
-            <div className="d-none">
-              <Editor
-                ref={editorRef}
-                content={summaryContent}
-                mode="read"
-                variant="ghost"
-              />
-            </div>
-            <div className="flex-fill text-truncate text-truncate-2 post-preview-content">
-              {summaryContentPlain}
-            </div>
-            <div className="d-flex align-items-center justify-content-center gap-24 mx-32">
-              {mediaURLs
-                .slice(0, MAX_NUMBER_MEDIA_DISPLAY)
-                .map((url, index) => (
-                  <div
-                    className={clsx("blog-post-image col-12 col-md-4 ", {
-                      "d-none d-md-block": index >= 1,
-                    })}
-                    key={url}
-                  >
-                    <Image
-                      alt=""
-                      objectFit="cover"
-                      ratio="16"
-                      className="rounded"
-                      src={url}
-                    />
-                    {(index === 0 || index === 2) &&
-                      mediaURLs.length - (index + 1) > 0 && (
-                        <div
-                          className={clsx(
-                            "position-absolute top-0 bottom-0 start-0 end-0 d-flex justify-content-center align-items-center rounded text-light bg-dark bg-opacity-50",
-                            {
-                              "d-flex d-md-none": index === 0,
-                              "d-none d-md-flex": index === 2,
-                            },
-                          )}
-                        >
-                          + {mediaURLs.length - (index + 1)}{" "}
-                          {t("post.preview.media")}
-                        </div>
-                      )}
-                  </div>
-                ))}
-            </div>
-            <div className="d-flex justify-content-between">
-              <div>
-                <div className="d-flex gap-12 align-items-center ">
-                  {showReactions && typeof reactions?.summary === "object" ? (
-                    <>
-                      <ReactionSummary summary={reactions.summary} />
-                      <span className="separator d-none d-md-block"></span>
-                    </>
-                  ) : null}
-                  {showViews && typeof views === "number" ? (
-                    <>
-                      <ViewsCounter viewsCounter={views} />
-                      <span className="separator d-none d-md-block"></span>
-                    </>
-                  ) : null}
-
-                  {typeof post.nbComments === "number" && (
-                    <div className="text-gray-700 d-flex align-items-center gap-8">
-                      <span>{post.nbComments}</span>
-                      <MessageInfo />
-                    </div>
-                  )}
-                </div>
-                {showReactions && typeof reactions?.summary === "object" ? (
-                  <ReactionChoice
-                    availableReactions={reactions.available}
-                    summary={reactions.summary}
-                  />
-                ) : null}
               </div>
-              <Button
-                variant="ghost"
-                rightIcon={<ArrowRight />}
-                color="secondary"
-                className="align-self-end"
-              >
-                {t("blog.post.preview.readMore")}
-              </Button>
+              <div className="text-gray-700 small column-gap-12 d-flex flex-column flex-md-row align-items-md-center">
+                <span>{post.author.username}</span>
+                <PostDate post={post} shortDisplay={true}></PostDate>
+              </div>
             </div>
           </div>
-        </Card.Body>
+          <Card.Body space="0">
+            <div className="d-flex flex-fill flex-column gap-16 pt-16">
+              <div className="d-none">
+                <Editor
+                  ref={editorRef}
+                  content={summaryContent}
+                  mode="read"
+                  variant="ghost"
+                />
+              </div>
+              <div className="flex-fill text-truncate text-truncate-2 post-preview-content">
+                {summaryContentPlain}
+              </div>
+              <div className="d-flex align-items-center justify-content-center gap-24 mx-32">
+                {mediaURLs
+                  .slice(0, MAX_NUMBER_MEDIA_DISPLAY)
+                  .map((url, index) => (
+                    <div
+                      className={clsx("blog-post-image col-12 col-md-4 ", {
+                        "d-none d-md-block": index >= 1,
+                      })}
+                      key={url}
+                    >
+                      <Image
+                        alt=""
+                        objectFit="cover"
+                        ratio="16"
+                        className="rounded"
+                        src={url}
+                      />
+                      {(index === 0 || index === 2) &&
+                        mediaURLs.length - (index + 1) > 0 && (
+                          <div
+                            className={clsx(
+                              "position-absolute top-0 bottom-0 start-0 end-0 d-flex justify-content-center align-items-center rounded text-light bg-dark bg-opacity-50",
+                              {
+                                "d-flex d-md-none": index === 0,
+                                "d-none d-md-flex": index === 2,
+                              },
+                            )}
+                          >
+                            + {mediaURLs.length - (index + 1)}{" "}
+                            {t("post.preview.media")}
+                          </div>
+                        )}
+                    </div>
+                  ))}
+              </div>
+              <div className="d-flex justify-content-between">
+                <div>
+                  <div className="d-flex gap-4 align-items-center ">
+                    {showReactions && typeof reactions?.summary === "object" ? (
+                      <>
+                        <ReactionSummary summary={reactions.summary} />
+                        <span className="separator d-none d-md-block"></span>
+                      </>
+                    ) : null}
+                    {showViews && typeof views === "number" ? (
+                      <>
+                        <ViewsCounter
+                          viewsCounter={views}
+                          onClick={handleViewsClick}
+                        />
+                        <span className="separator d-none d-md-block"></span>
+                        {viewsModalOpen && (
+                          <ViewsModal
+                            viewsDetails={viewsDetails!}
+                            isOpen={viewsModalOpen}
+                            onModalClose={handleViewsModalClose}
+                          />
+                        )}
+                      </>
+                    ) : null}
+
+                    {typeof post.nbComments === "number" && (
+                      <div className="text-gray-700 d-flex align-items-center gap-8 p-8 post-preview-comment-icon">
+                        <span>{post.nbComments}</span>
+                        <MessageInfo />
+                      </div>
+                    )}
+                  </div>
+                  {showReactions && typeof reactions?.summary === "object" ? (
+                    <ReactionChoice
+                      availableReactions={reactions.available}
+                      summary={reactions.summary}
+                    />
+                  ) : null}
+                </div>
+                <Button
+                  variant="ghost"
+                  rightIcon={<ArrowRight />}
+                  color="secondary"
+                  className="align-self-end"
+                >
+                  {t("blog.post.preview.readMore")}
+                </Button>
+              </div>
+            </div>
+          </Card.Body>
+        </div>
       </Card>
       {blog && post && (
         <PostPreviewActionBar
